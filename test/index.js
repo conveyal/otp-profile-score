9ae1470d6d181e791825b0ec2dc4b6849b25c3ba
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-05-29 06:36:58 -0700

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,17 +1,19 @@
-var assert = require('assert');
-var data = require('./profile.json');
-var Scorer = require('..');
+/* global describe, it */
 
-describe('OTP Profile Score', function() {
-  describe('constructor', function() {
-    it('should have default rates and factors', function() {
-      var scorer = new Scorer();
-      assert(scorer.factors !== undefined);
-      assert(scorer.rates !== undefined);
-    });
+var assert = require('assert')
+var data = require('./profile.json')
+var Scorer = require('..')
+
+describe('OTP Profile Score', function () {
+  describe('constructor', function () {
+    it('should have default rates and factors', function () {
+      var scorer = new Scorer()
+      assert(scorer.factors !== undefined)
+      assert(scorer.rates !== undefined)
+    })
 
     it('should override default rates and factors with passed in values',
-      function() {
+      function () {
         var scorer = new Scorer({
           factors: {
             bikeParking: 2
@@ -19,45 +21,45 @@ describe('OTP Profile Score', function() {
           rates: {
             bikeSpeed: 5
           }
-        });
+        })
 
-        assert(scorer.factors.bikeParking === 2);
-        assert(scorer.rates.bikeSpeed === 5);
-        assert(scorer.factors.calories === -0.01);
-      });
-  });
+        assert(scorer.factors.bikeParking === 2)
+        assert(scorer.rates.bikeSpeed === 5)
+        assert(scorer.factors.calories === -0.01)
+      })
+  })
 
-  describe('processOption', function() {
-    it('should tally and score an option', function() {
-      var scorer = new Scorer();
-      var option = scorer.processOption(data.options[0]);
-      assert(option.score === 37.955000000000005, option.score);
-    });
+  describe('processOption', function () {
+    it('should tally and score an option', function () {
+      var scorer = new Scorer()
+      var option = scorer.processOption(data.options[0])
+      assert(option.score === 39.95, option.score)
+    })
 
-    it('should handle factors as functions', function() {
+    it('should handle factors as functions', function () {
       var scorer = new Scorer({
         factors: {
-          calories: function(c) {
-            if (c > 100) return 0;
-            else return c * -0.01;
+          calories: function (c) {
+            if (c > 100) return 0
+            else return c * -0.01
           }
         }
-      });
-      var option = scorer.processOption(data.options[0]);
-      assert(option.score === 41.95, option.score);
-    });
-  });
+      })
+      var option = scorer.processOption(data.options[0])
+      assert(option.score === 39.95, option.score)
+    })
+  })
 
-  describe('processOptions', function() {
-    it('should tally and score all access modes for all options', function() {
-      var scorer = new Scorer();
-      var options = scorer.processOptions(data.options);
-      assert(options.length === 12, options.length);
+  describe('processOptions', function () {
+    it('should tally and score all access modes for all options', function () {
+      var scorer = new Scorer()
+      var options = scorer.processOptions(data.options)
+      assert(options.length === 12, options.length)
 
       for (var i = 0; i < options.length; i++) {
-        assert(options[i].score !== undefined);
-        assert(!isNaN(options[i].score));
+        assert(options[i].score !== undefined)
+        assert(!isNaN(options[i].score))
       }
-    });
-  });
-});
+    })
+  })
+})
